20f630ce2080c36b913289782dcd08709db93cc4
Daniel-Loaiza <danielloaizaisi@gmail.com>
2026-01-18 21:55:10 -0500

    feat: initialize NestJS application with CORS and global validation pipe.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // Frontend URL
+    origin: [
+      'http://localhost:5173',
+      'https://ticket-app-antigravity-1.onrender.com',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
